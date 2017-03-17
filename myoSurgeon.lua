74d13da6f48f128f32275db4f2cc2d433f8444b7
Robert Bryant <Robert Bryant>
2017-03-17 12:21:25 +0000

    Rolling sort of working...

--- myoSurgeon.lua
+++ myoSurgeon.lua
@@ -2,21 +2,27 @@ scriptId = 'com.myosurgeon'
 scriptTitle = "MyoSurgeon"  
 scriptDetailsUrl = ""  
 
-
+mouseDown = false
 
 function onForegroundWindowChange(app, title)
     local uppercaseApp = string.upper(app)
-    myo.debug(uppercaseApp)
     return platform == "Windows" and uppercaseApp == "SS2013.EXE"
 end
 
 function onPoseEdge(pose, edge)
     if pose == "fist" and edge == "on" then doGrab()
     elseif pose == "fist" and edge == "off" then doRelease()
+    elseif pose == "fingersSpread" and edge == "on" then dropHand()
+    elseif pose == "fingersSpread" and edge == "off" then raiseHand()
     end
 end
 
 function onPeriodic()
+    local r = myo.getRoll()
+    if r < -1 or r > 1 then startRoll()
+    else endRoll()
+    end
+
 end
 
 
@@ -26,9 +32,44 @@ end
 
 function onActiveChange(isActive)
     if isActive == true
-       then myo.controlMouse(true)
+      then activate()
     else
-        myo.controlMouse(false)
+        deactivate()
+    end
+end
+
+function activate()
+    myo.controlMouse(true)
+    myo.setLockingPolicy("none")
+end
+
+function deactivate()
+    myo.controlMouse(false)
+    myo.setLockingPolicy("standard")
+end
+
+function dropHand()
+    myo.mouse("left", "down")
+    mouseDown = true
+end
+
+function raiseHand()
+    myo.mouse("left", "up")
+    mouseDown = false
+end
+
+function startRoll()
+    if mouseDown == false 
+        then myo.mouse("right", "down")
+        mouseDown = true
+    end
+end
+
+function endRoll()
+    if mouseDown == true 
+        then 
+        myo.mouse("right", "up") 
+        mouseDown = false
     end
 end
 
@@ -38,7 +79,7 @@ function doGrab()
     myo.keyboard("w","down")
     myo.keyboard("e","down")
     myo.keyboard("r","down")
-    myo.keyboard("spacebar","down")
+    myo.keyboard("space","down")
 end
 
 function doRelease()
@@ -46,5 +87,5 @@ function doRelease()
     myo.keyboard("w","up")
     myo.keyboard("e","up")
     myo.keyboard("r","up")
-    myo.keyboard("spacebar","up")
+    myo.keyboard("space","up")
 end
